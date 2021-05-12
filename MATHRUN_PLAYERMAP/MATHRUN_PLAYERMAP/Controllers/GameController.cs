19c6041ce82eeffb1a73ebec5d46b4b247d05b4b
predeinnikita <54530655+Nikita4513@users.noreply.github.com>
2021-05-12 18:22:01 +0500

    improved addding sprites

--- MATHRUN_PLAYERMAP/MATHRUN_PLAYERMAP/Controllers/GameController.cs
+++ MATHRUN_PLAYERMAP/MATHRUN_PLAYERMAP/Controllers/GameController.cs
@@ -24,29 +24,35 @@ namespace MATHRUN_PLAYERMAP.Controllers
             {
                 for (var y = 0; y < Game.Field.Height; y++)
                 {
-                    if (Game.Field.Map[x, y] != null && Game.Field.Map[x, y].Name == "Player")
-                    {
-                        var playerImage = new Bitmap("C:\\Users\\DNS\\Documents\\GitHub\\MATH-RUN_MAIN\\MATHRUN_PLAYERMAP\\MATHRUN_PLAYERMAP\\Sprites\\Player\\player_01.png");
-                        g.DrawImage(new Bitmap(playerImage, new Size(CellSize, CellSize)), x*CellSize, y*CellSize);
-                    }
-                    if (Game.Field.Map[x, y] != null && Game.Field.Map[x, y].Name == "Monster")
-                    {
-                        var playerImage = new Bitmap("C:\\Users\\DNS\\Documents\\GitHub\\MATH-RUN_MAIN\\MATHRUN_PLAYERMAP\\MATHRUN_PLAYERMAP\\Sprites\\Monster\\monster_01.png");
-                        g.DrawImage(new Bitmap(playerImage, new Size(CellSize, CellSize)), x * CellSize, y * CellSize);
-                    }
                     if (Game.Field.Map[x, y] != null && Game.Field.Map[x, y].Name == "Wall")
                     {
-                        var playerImage = new Bitmap("C:\\Users\\DNS\\Documents\\GitHub\\MATH-RUN_MAIN\\MATHRUN_PLAYERMAP\\MATHRUN_PLAYERMAP\\Sprites\\Blocks\\Block_01.png");
-                        g.DrawImage(new Bitmap(playerImage, new Size(CellSize, CellSize)), x * CellSize, y * CellSize);
+                        var wallImage = Resource.wall;
+                        g.DrawImage(new Bitmap(wallImage, new Size(CellSize, CellSize)), x * CellSize, y * CellSize);
                     }
                     if (Game.Field.Map[x, y] != null && Game.Field.Map[x, y].Name == "Finish")
                     {
-                        var playerImage = new Bitmap("C:\\Users\\DNS\\Documents\\GitHub\\MATH-RUN_MAIN\\MATHRUN_PLAYERMAP\\MATHRUN_PLAYERMAP\\Sprites\\Ground\\Ground_06.png");
+                        var finishImage = Resource.finish;
+                        g.DrawImage(new Bitmap(finishImage, new Size(CellSize, CellSize)), x * CellSize, y * CellSize);
+                    }
+                    if (Game.Field.Map[x, y] != null && Game.Field.Map[x, y].Name == "Player")
+                    {
+                        var playerImage = Resource.player;
                         g.DrawImage(new Bitmap(playerImage, new Size(CellSize, CellSize)), x * CellSize, y * CellSize);
+                    }
+                    if (Game.Field.Map[x, y] != null && Game.Field.Map[x, y].Name == "Monster")
+                    {
+                        var monsterImage = Resource.monster;
+                        g.DrawImage(new Bitmap(monsterImage, new Size(CellSize, CellSize)), x * CellSize, y * CellSize);
                     }
                 }
             }
         }
+
+        public void DrawQuestion(Graphics g)
+        {
+            g.DrawRectangle(new Pen(Color.Red), new Rectangle(new Point(10, 10), new Size(10, 10)));
+        }
+
 
     }
 }
