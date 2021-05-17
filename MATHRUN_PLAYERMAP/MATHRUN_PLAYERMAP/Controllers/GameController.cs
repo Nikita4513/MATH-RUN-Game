29d0e6e1057f0f7c5883fffc0ca41e3bd8f778f2
predeinnikita <54530655+Nikita4513@users.noreply.github.com>
2021-05-17 15:44:08 +0500

    Added draws
    added drawing question
    added drawing scores
    added checking state of game

--- MATHRUN_PLAYERMAP/MATHRUN_PLAYERMAP/Controllers/GameController.cs
+++ MATHRUN_PLAYERMAP/MATHRUN_PLAYERMAP/Controllers/GameController.cs
@@ -1,10 +1,12 @@
 ﻿using Domains_MATH_RUN;
+using Domains_MATH_RUN.Domains;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace MATHRUN_PLAYERMAP.Controllers
 {
@@ -18,8 +20,9 @@ namespace MATHRUN_PLAYERMAP.Controllers
             Game = new Game();
         }
 
-        public void DrawMap(Graphics g)
+        public void DrawMap(Graphics g, GameForm gameForm)
         {
+            CheckGameState(gameForm);
             for (var x = 0; x < Game.Field.Width; x++)
             {
                 for (var y = 0; y < Game.Field.Height; y++)
@@ -46,13 +49,83 @@ namespace MATHRUN_PLAYERMAP.Controllers
                     }
                 }
             }
+            DrawQuestion(g);
         }
 
         public void DrawQuestion(Graphics g)
         {
-            g.DrawRectangle(new Pen(Color.Red), new Rectangle(new Point(10, 10), new Size(10, 10)));
+            //g.DrawRectangle(new Pen(Color.Red), new Rectangle(new Point(10, 10), new Size(10, 10)));
+            g.DrawString(
+                "Вопрос: " + Game.CurrentQuestion.Value + "\t\t Очки: " + Game.Scores,
+                new Font("Arial", 15),
+                new SolidBrush(Color.Aqua),
+                new Rectangle(Game.Field.Width * CellSize / 3 , Game.Field.Height * CellSize - 2 * CellSize + 5, Game.Field.Width * CellSize, CellSize));
+
+            g.DrawString(
+                Game.CurrentQuestion.PossibleAnswers[0] + "\t\t" + Game.CurrentQuestion.PossibleAnswers[1] + "\t\t" + Game.CurrentQuestion.PossibleAnswers[2],
+                new Font("Arial", 15),
+                new SolidBrush(Color.White),
+                new Rectangle(Game.Field.Width * CellSize / 4, Game.Field.Height * CellSize - CellSize, Game.Field.Width * CellSize, CellSize));
         }
 
+        public void CheckAnswer(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    if (Game.CurrentQuestion.IsRightAnswer(Game.CurrentQuestion.PossibleAnswers[0]))
+                    {
+                        Game.Scores++;
+                        Game.Player.MoveNext();
+                    }
+                    else
+                        Game.Scores--;
+                    break;
+                case Keys.Up:
+                    if (Game.CurrentQuestion.IsRightAnswer(Game.CurrentQuestion.PossibleAnswers[1]))
+                    {
+                        Game.Scores++;
+                        Game.Player.MoveNext();
+                    }
+                    else
+                        Game.Scores--;
+                    break;
+                case Keys.Right:
+                    if (Game.CurrentQuestion.IsRightAnswer(Game.CurrentQuestion.PossibleAnswers[2]))
+                    {
+                        Game.Scores++;
+                        Game.Player.MoveNext();
+                    }
+                    else
+                        Game.Scores--;
+                    break;
+                default:
+                    return;
+            }
+            Game.GetNextQuestion();
+        }
+
+        private void CheckGameState(GameForm gameForm)
+        {
+            if (Game.Scores <= 0)
+                gameForm.Init();
+            try
+            {
+                Game.Field.GetLocationOf(typeof(Player));
+            }
+            catch
+            {
+                gameForm.Init();
+            }
+            try
+            {
+                Game.Field.GetLocationOf(typeof(Finish));
+            }
+            catch
+            {
+                gameForm.InitNextLevel();
+            }
+        }
 
     }
 }
