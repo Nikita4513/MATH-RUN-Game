cea7c1bf1256b737505f09d230429d35cb4cdd8c
predeinnikita <54530655+Nikita4513@users.noreply.github.com>
2021-05-17 15:40:30 +0500

    cleaning code

--- MATHRUN_PLAYERMAP/MATHRUN_PLAYERMAP/GameForm.cs
+++ MATHRUN_PLAYERMAP/MATHRUN_PLAYERMAP/GameForm.cs
@@ -1,65 +1,78 @@
-﻿using Domains_MATH_RUN;
-using MATHRUN_PLAYERMAP.Controllers;
+﻿using MATHRUN_PLAYERMAP.Controllers;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MATHRUN_PLAYERMAP
 {
     public partial class GameForm : Form
     {
-        //public Image Monster;
-        private static Image Background;
         private GameController gameController;
+
         public GameForm()
         {
             InitializeComponent();
-            timer.Interval = 10000000;
-            
-            timer.Tick += new EventHandler(Update);
+            timer.Interval = 1000;
+            timerMonster.Interval = 110;
 
-            //KeyDown += new KeyEventHandler(OnPress);
-            //KeyUp += new KeyEventHandler(OnKeyUp);
+            timer.Tick += new EventHandler(UpdateMonster);
+            //timerMonster.Tick += new EventHandler(UpdateMonster);
 
+            KeyDown += new KeyEventHandler(OnPress);
+            //KeyUp += new KeyEventHandler(Update);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer
+                | ControlStyles.AllPaintingInWmPaint
+                | ControlStyles.UserPaint, true);
+            UpdateStyles();
             Init();
         }
 
-        
-        
         public void OnPaint(object sender, PaintEventArgs e)
         {
-            Graphics g = e.Graphics;
-            //g.DrawImage(Background, 10, 10);
-            gameController.DrawMap(g);
+            var graphic = e.Graphics;
+            gameController.DrawMap(graphic, this);
+            //gameController.DrawQuestion(graphic);
         }
 
-        private void Init()
+
+        public void Init()
         {
             gameController = new GameController();
-            gameController.Game.ChangeOnNextLevel();
-            gameController.Game.ChangeOnNextLevel();
-            gameController.Game.ChangeOnNextLevel();
-
-            var directoryInfo = new DirectoryInfo(Directory.GetCurrentDirectory());
-            var cellImage = new Bitmap("C:\\Users\\DNS\\Documents\\GitHub\\MATH-RUN_MAIN\\MATHRUN_PLAYERMAP\\MATHRUN_PLAYERMAP\\Sprites\\Ground\\ground_01.png");
-            Background = new Bitmap(cellImage, new Size(gameController.CellSize, gameController.CellSize));
-            BackgroundImage = Background;
-            Size = new Size(gameController.Game.Field.Width * gameController.CellSize + 20, gameController.Game.Field.Height * gameController.CellSize + 50);
-            this.MaximumSize = Size;
-            this.MinimumSize = Size;
+            
+            BackgroundImage = new Bitmap(Resource.ground, new Size(gameController.CellSize, gameController.CellSize)); 
+            Size = new Size(gameController.Game.Field.Width * gameController.CellSize + 20,
+                            gameController.Game.Field.Height * gameController.CellSize + 50);
+            //this.MaximumSize = Size;
+            //this.MinimumSize = Size;
             timer.Start();            
         }
 
-        public void Update(object sender, EventArgs e)
+        public void InitNextLevel()
         {
+            gameController.Game.ChangeOnNextLevel();
+            BackgroundImage = new Bitmap(Resource.ground, new Size(gameController.CellSize, gameController.CellSize));
+            Size = new Size(gameController.Game.Field.Width * gameController.CellSize + 20,
+                            gameController.Game.Field.Height * gameController.CellSize + 50);
+            //this.MaximumSize = Size;
+            //this.MinimumSize = Size;
+            timer.Start();
+        }
 
+        public void UpdatePlayer(object sender, EventArgs e)
+        {
+            gameController.Game.Player.MoveNext();
+            Invalidate();
+        }
+
+        public void UpdateMonster(object sender, EventArgs e)
+        {
+            gameController.Game.Monster.MoveNext();
+            Invalidate();
+        }
+
+        public void OnPress(object sender, KeyEventArgs e)
+        {
+            gameController.CheckAnswer(sender, e);
             Invalidate();
         }
     }
