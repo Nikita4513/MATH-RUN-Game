08ab1fb5794dacec539a3441b729f7ad757b56c0
predeinnikita <54530655+Nikita4513@users.noreply.github.com>
2021-05-18 11:10:47 +0500

    Added menu

---
+++ MATHRUN_PLAYERMAP/MATHRUN_PLAYERMAP/MenuForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MATHRUN_PLAYERMAP
+{
+    public partial class MenuForm : Form
+    {
+        public MenuForm()
+        {
+            InitializeComponent();
+
+            this.Size = new Size(1000, 600);
+            this.MaximumSize = Size;
+            this.MinimumSize = Size;
+
+            StartButton.Click += (sender, args) =>
+            {
+                var gameForm = new GameForm(this);
+                gameForm.Show(); // отображаем Form2
+                this.Hide();
+                //while (gameForm.Is)
+            };
+
+            SettingsButton.Click += (sender, args) =>
+            {
+
+            };
+
+            ExitButton.Click += (sender, eventArgs) => Close();
+
+            //AboutGameButton.Click += (sender, eventArgs) => new AboutGameForm().Show();
+
+
+            FormClosing += (sender, eventArgs) =>
+            {
+                var result = MessageBox.Show("Действительно закрыть?", "", MessageBoxButtons.YesNo,
+                    MessageBoxIcon.Question);
+                if (result != DialogResult.Yes)
+                    eventArgs.Cancel = true;
+            };
+        }
+    }
+}
--- MATHRUN_PLAYERMAP/MATHRUN_PLAYERMAP/Program.cs
+++ MATHRUN_PLAYERMAP/MATHRUN_PLAYERMAP/Program.cs
@@ -17,7 +17,7 @@ namespace MATHRUN_PLAYERMAP
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new GameForm());
+            Application.Run(new MenuForm());
         }
     }
 }
